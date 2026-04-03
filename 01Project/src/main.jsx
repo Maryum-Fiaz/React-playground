57ef086ab417ccfdfc892b3b688c2079176281e7
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-04-03 21:38:00 +0500

    instead of JSX, used createElement

--- 01Project/src/main.jsx
+++ 01Project/src/main.jsx
@@ -1,9 +1,26 @@
-import { StrictMode } from 'react'
+import {createElement, StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 
+
+const pVal = document.getElementsByClassName('greetings');
+let user;
+
+if(pVal.innerText === 'Hey there'){
+  user = 'Nina'
+} else {
+  user = 'Anna'
+}
+const reactElement = createElement(
+  'p',
+  {className : 'greetings'},
+  'Hey there, ',
+  user
+)
+
 createRoot(document.getElementById('loadedfries')).render(
   <StrictMode>
-    <App />
+    {reactElement}
+    {/* <App /> */}
   </StrictMode>,
 )
