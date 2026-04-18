c9e613a865be951d133917812542bf76a601e725
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-04-18 19:55:20 +0500

    reusable component InputBox

--- 01Project/src/smallProjects/language-translator/FinalBox.jsx
+++ 01Project/src/smallProjects/language-translator/FinalBox.jsx
@@ -4,8 +4,14 @@ import InputBox from './InputBox'
 function FinalBox() {
   return (
     <div className='h-screen flex justify-center items-center bg-gray-200'>
-        <div className='w-full max-w-md mx-auto rounded-lg shadow-lg'>
-        <InputBox />
+        <div className='w-full max-w-md mx-auto bg-white rounded-lg shadow-lg'>
+        <InputBox label="Source Language" />
+
+        
+        <hr className='my-1.5 mx-3 text-gray-400' />
+        
+
+        <InputBox label="Target Language" isDisabled color='bg-gray-100' selectLanguage='Spanish' />
 
         </div>
     </div>
--- 01Project/src/smallProjects/language-translator/InputBox.jsx
+++ 01Project/src/smallProjects/language-translator/InputBox.jsx
@@ -1,30 +1,53 @@
-import React from "react";
+import React, { useId } from "react";
 
-function InputBox() {
+function InputBox({
+  label,
+  sentence,
+  onSentenceChange,
+  onLangChange,
+  languageOptions = [],
+  selectLanguage = "English",
+  isDisabled = false,
+  color = "bg-white",
+}) {
+
+  const sentenceInputId = useId();
+  console.log('ididid: ', sentenceInputId)
+
+
   return (
     <div className="p-3 bg-white rounded-lg text-sm">
       <div className="flex justify-between py-1.5">
         <label
-          htmlFor=""
+          htmlFor={sentenceInputId}
           className="inline-block font-semibold text-gray-700 font-mono"
         >
-          Source Language
+          {label}
         </label>
-        <select name="" id="" className="border border-gray-300 rounded px-1.5 focus:outline-none focus:ring-2 focus:ring-gray-500">
-          <option value="">English</option>
-          <option value="">Urdu</option>
-          <option value="">Arabic</option>
-          <option value="">Spanish</option>
+
+        <select
+          name=""
+          id={sentenceInputId}
+          className="border border-gray-300 rounded px-1.5 focus:outline-none focus:ring-2 focus:ring-gray-500"
+          value={selectLanguage}
+          onChange={(e) => onLangChange && onLangChange(e.target.value)}
+        >
+          {languageOptions.map((language) => (
+            <option key={language} value={language}>
+              {language}
+            </option>
+          ))}
         </select>
       </div>
 
       <div>
         <textarea
-          name=""
-          id=""
           rows="4"
           placeholder="Type your text here..."
-          className="border border-gray-300 rounded-lg w-full bg-transparent p-1.5 scroll-auto focus:outline-none focus:ring-2 focus:ring-gray-500"
+          className={`border border-gray-300 rounded-lg w-full p-1.5 scroll-auto focus:outline-none focus:ring-2 focus:ring-gray-500 ${color}`}
+          disabled={isDisabled}
+          value={sentence}
+          onChange={(e) => onSentenceChange && onSentenceChange(e.target.value)}
         ></textarea>
       </div>
     </div>
