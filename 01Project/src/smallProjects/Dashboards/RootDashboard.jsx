8eed5599ece096616a997597f54cdecda1919895
Maryum Fiaz <maryumfiazbaig333@gmail.com>
2026-04-25 20:11:55 +0500

    Dashboard routing working

--- 01Project/src/smallProjects/Dashboards/RootDashboard.jsx
+++ 01Project/src/smallProjects/Dashboards/RootDashboard.jsx
@@ -2,22 +2,22 @@ import React from 'react'
 import Dashboard from '../../components/Dashboard'
 import OrderDashboard from '../../components/OrderDashboard'
 import TopSelling from '../../components/TopSelling'
-import { Link } from 'react-router'
+import { Link, Outlet } from 'react-router'
 
 export default function RootDashboard() {
 
 
     const components = [
         {label: "Active Users", component: <Dashboard />, address: "/dashboard/activeUsers"},
-        {label: "Order Summary", component: <OrderDashboard />, address: "/orderSummary"},
-        {label: "Top Selling", component: <TopSelling />, address: "/topSelling"},
+        {label: "Order Summary", component: <OrderDashboard />, address: "/dashboard/orderSummary"},
+        {label: "Top Selling", component: <TopSelling />, address: "/dashboard/topSelling"},
     ]
 
 
   return (
     <>
     <div className='flex justify-center gap-2 bg-mauve-200 p-4 rounded-b-md'>
-        {components.map(({label, component, address}) => (
+        {components.map(({label, address}) => (
             <button key={label} className='bg-mauve-600 text-mauve-50 border-2 border-mauve-800 rounded-lg py-2 px-2'>
                 <Link to={address}>
                     {label}
@@ -25,7 +25,7 @@ export default function RootDashboard() {
             </button>
         ))}
     </div>
-
+        <Outlet />
     </>
   )
 }
